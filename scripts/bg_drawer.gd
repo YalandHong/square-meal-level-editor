feeb8e72d1882463aaf62e652182ab01b91a365b
YalandHong <yalandhong@foxmail.com>
2024-09-22 15:19:39 +0800

    fix bg drawer

--- scripts/bg_drawer.gd
+++ scripts/bg_drawer.gd
@@ -8,18 +8,18 @@ var draw_height: int
 var background_texture: Texture2D
 
 func _draw() -> void:
-    if background_texture:
-        var texture_size: Vector2 = background_texture.get_size()
+    var texture_size: Vector2 = background_texture.get_size()
 
-        # 计算需要横向和纵向绘制的次数
-        var rows: int = ceil(draw_width / texture_size.y)
-        var cols: int = ceil(draw_height / texture_size.x)
+    # 计算需要横向和纵向绘制的次数
+    var rows: int = ceil(draw_height / texture_size.y)
+    var cols: int = ceil(draw_width / texture_size.x)
 
-        # 循环绘制平铺背景
-        for row in range(rows):
-            for col in range(cols):
-                var pos: Vector2 = Vector2(col * texture_size.x, row * texture_size.y)
-                draw_texture(background_texture, pos)
+    # 循环绘制平铺背景
+    for row in range(rows):
+        for col in range(cols):
+            var pos: Vector2 = Vector2(col * texture_size.x, row * texture_size.y)
+            draw_texture(background_texture, pos)
+            print("draw bg at ", pos)
 
 func _ready() -> void:
     var game_manager: GameManager = get_node("../GameManager")
