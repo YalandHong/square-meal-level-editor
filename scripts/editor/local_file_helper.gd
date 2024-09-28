632b5a2fd415b85ae7cfcc73842fe878d7adf779
YalandHong <yalandhong@foxmail.com>
2024-09-28 11:08:14 +0800

    save file helper

--- scripts/editor/local_file_helper.gd
+++ scripts/editor/local_file_helper.gd
@@ -4,8 +4,8 @@ class_name LocalFileHelper
 static func read_level_map_txt_file(file_path: String) -> Array:
     var file = FileAccess.open(file_path, FileAccess.READ)
 
-    if not file:
-        print("Error opening file!")
+    if file == null:
+        printerr("Error opening file: ", FileAccess.get_open_error())
         return []
 
     var result_array: Array = []
@@ -26,3 +26,22 @@ static func read_level_map_txt_file(file_path: String) -> Array:
 
     file.close()
     return result_array
+
+# 静态函数保存 level_map 为 TSV 文件
+static func save_level_map_to_tsv_file(level_map: Array, file_path: String) -> void:
+    var file = FileAccess.open(file_path, FileAccess.WRITE)
+
+    # 尝试打开文件进行写入
+    if file == null:
+        printerr("Error opening file: ", FileAccess.get_open_error())
+        return
+
+    # 遍历 level_map 的每一行
+    for row in level_map:
+        # 将行转换为字符串，并用制表符连接每个元素
+        var line = row.join("\t")
+        file.store_line(line)
+
+    # 关闭文件
+    file.close()
+    print("Level map saved to: ", file_path)
