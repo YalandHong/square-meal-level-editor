f02add77fb87c6b20f2d1552f6a39e7356fd0b7d
YalandHong <yalandhong@foxmail.com>
2024-10-04 11:14:15 +0800

    rename score font to counting font

---
+++ scripts/menu/counting_font_label.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+class_name CountingFontLabel
+
+var displayed_text: String
+
+func _init(text: String) -> void:
+    displayed_text = text
+
+func _draw() -> void:
+    draw_pixel_text_centered(displayed_text)
+
+func _process(_delta: float) -> void:
+    queue_redraw()
+
+# 在指定相对坐标绘制文本
+func display_special_text(x: float, y: float, text: String, font_map: Dictionary):
+    text = text.to_upper()
+    var pos = Vector2(x, y)
+    for ch in text:
+        draw_texture(font_map[ch], pos)
+        pos.x += font_map[ch].get_width() - 2  # 移动到下一个字符的位置
+
+# 居中显示文本
+func draw_special_text_centered(text: String, font_map: Dictionary):
+    text = text.to_upper()
+    var total_width = 0
+    for ch in text:
+        if ch in font_map:
+            total_width += font_map[ch].get_width() - 2  # 计算总宽度
+    var start_x = -total_width / 2  # 计算起始位置
+    var total_height = font_map[text[0]].get_height()
+    var start_y = -total_height / 2
+    display_special_text(start_x, start_y, text, font_map)
+
+func draw_pixel_text_centered(text: String):
+    draw_special_text_centered(text, PixelFont.font_map_counting)
--- scripts/menu/pixel_font.gd
+++ scripts/menu/pixel_font.gd
@@ -3,6 +3,7 @@ extends Node2D
 # 字体映射，保存字符与对应纹理的关系
 var font_map_active: Dictionary = {}
 var font_map_inactive: Dictionary = {}
+var font_map_counting: Dictionary = {}
 
 # 初始化，加载所有字符的纹理
 func _ready():
@@ -15,3 +16,7 @@ func load_fonts():
         font_map_active[ch] = load(texture_path)
         texture_path = "res://sprites/font/inactive/" + ch + ".png"
         font_map_inactive[ch] = load(texture_path)
+
+    for ch in "0123456789":
+        var texture_path = "res://sprites/font/score_font/" + ch + ".png"
+        font_map_counting[ch] = load(texture_path)
